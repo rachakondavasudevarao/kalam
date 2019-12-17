d99a4f15028005adec2dba9e8d5df712efdc6387
vasu <vasudevarao476@gmail.com>
2019-12-17 20:30:58 +0530

    testing

---
+++ src/main/resources/sampleservlet.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+/**
+
+ * Servlet implementation class SimpleServlet
+
+ */
+
+@WebServlet("/hello")
+public class sampleservlet {
+	 private static final long serialVersionUID = 1L;
+
+	   @Override
+
+	   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+
+	         throws ServletException, IOException {
+
+	      resp.setContentType("text/plain");
+
+	      resp.getWriter().write("Hello World! Maven Web Project Example.");
+
+	   }
+}
